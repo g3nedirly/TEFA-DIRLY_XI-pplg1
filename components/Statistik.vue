ddc43447153a18aee68f701667e841a463fb3685
g3nedirly <gon3dirly@gmail.com>
2024-05-21 12:51:37 +0700

    Add files via upload

---
+++ components/Statistik.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+
+<script setup>
+import Chart from 'chart.js/auto';
+
+const labels = [
+  'Januari',
+  'Februari',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(211, 211, 211)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [13, 19, 22, 17, 18, 20, 30, 35],
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    Options: {}
+  };
+
+onMounted(() => {
+  const myChart = new Chart (
+    document.getElementById('myChart'),
+    config
+  );
+})
+</script>
+
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div cllass="container-fluid">
+        <Header />
+        <slot />
+    </div>
+</template>
